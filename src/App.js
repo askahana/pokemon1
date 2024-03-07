5ae699ae1df2341030ba94287b144e8cf0e91bbe
hanadasuka <hanadasuka@gmail.com>
2024-03-07 16:05:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+async function fetchData() {
+  const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonSprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <input type="text" id="pokemonName" placeholder="Enter pokemon name"/>
+    <button onClick={fetchData}>Fetch Pokemon</button>
+    <img src="" alt="Pokemon Sprite" id="pokemonSprite" style={{ display: 'none' }}/>
+
     </div>
   );
 }
